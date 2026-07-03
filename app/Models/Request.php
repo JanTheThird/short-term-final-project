d35f8b1d7072efde16d91602f24399fcd10d8c3e
JanTheThird <20122613@s.ubaguio.edu>
2026-07-03 15:25:21 +0800

    feat : Request  Migration and Model

---
+++ app/Models/Request.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Request extends Model
+{
+    /** @use HasFactory<\Database\Factories\RequestFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'patient_id',
+        'therapist_id',
+        'concern',
+        'status',
+    ];
+}
