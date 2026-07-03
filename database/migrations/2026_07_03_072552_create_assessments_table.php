e6062f860d13c6f5cc93abc63cc897c9fc24d35c
JanTheThird <20122613@s.ubaguio.edu>
2026-07-03 15:27:39 +0800

    feat : Assessment  Migration and Model

---
+++ database/migrations/2026_07_03_072552_create_assessments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assessments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->foreignId('request_id')->constrained('requests');
+
+            $table->integer('pain_level');
+            $table->integer('mobility_level');
+            $table->integer('strength_level');
+            $table->integer('flexibility_level');
+            $table->text('remarks_level');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assessments');
+    }
+};
